03c1804eef1ce0b3427fc37592421ec705162048
w-kon <wkon@yandex.ru>
2024-08-31 18:10:01 +0600

    first commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        BonusService service = new BonusService();
+
+        long bonus = service.calculate(5000, true);
+        System.out.print(bonus);
+
+    }
+}
---
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,10 @@
+package $ru.netology.DZ;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
